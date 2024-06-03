3851a81b7a0b8649452c3c39aa142c26639ff0be
Mateusz Martyna <mateuszmartyna44@gmail.com>
2024-06-03 21:55:17 +0200

    Transactions api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,14 +4,16 @@ import { HTTPException } from "hono/http-exception";
 
 import accounts from "./accounts";
 import categories from "./categories";
+import transactions from "./transactions";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
 const routes = app
-    .route("/accounts", accounts)
-    .route("/categories", categories);
+  .route("/accounts", accounts)
+  .route("/categories", categories)
+  .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
