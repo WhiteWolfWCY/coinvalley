158c3033a37e1526d0e02ff5fb54c93f82bf2e7d
WhiteWolfWCY <mateuszmartyna44@gmail.com>
2024-06-04 08:39:35 +0200

    Transactions hooks

---
+++ features/transactions/api/use-edit-transaction.ts
@@ -0,0 +1,36 @@
+import { toast } from "sonner";
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.transactions)[":id"]["$patch"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.transactions)[":id"]["$patch"]
+>["json"];
+
+export const useEditTransaction = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.transactions[":id"]["$patch"]({
+        param: { id },
+        json,
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Your transaction has been updated!");
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+      queryClient.invalidateQueries({ queryKey: ["transaction", { id }] });
+    },
+    onError: () => {
+      toast.error("Failed to edit a transaction!");
+    },
+  });
+
+  return mutation;
+};
