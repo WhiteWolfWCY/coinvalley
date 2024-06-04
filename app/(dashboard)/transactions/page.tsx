3488ddd23accbb388c572f01536ee707c6dbd857
WhiteWolfWCY <mateuszmartyna44@gmail.com>
2024-06-04 11:16:24 +0200

    Transactions form

---
+++ app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Loader2, Plus } from "lucide-react";
+
+import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete-accounts";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/DataTable";
+
+import { columns } from "./columns";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const TransactionsPage = () => {
+  const newTransaction = useNewTransaction();
+  const accountsQuery = useGetAccounts();
+  const deleteAccounts = useBulkDeleteAccounts()
+  const accounts = accountsQuery.data || [];
+
+  const isDisabled = accountsQuery.isLoading || deleteAccounts.isPending
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+            <CardContent>
+              <div className="h-[500px] w-full flex items-center justify-center">
+                <Loader2 className="size-6 text-slate-300 animate-spin" />
+              </div>
+            </CardContent>
+          </CardHeader>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Transaction history</CardTitle>
+          <Button onClick={newTransaction.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id);
+              deleteAccounts.mutate({ ids })
+            }}
+            filterKey="name"
+            columns={columns}
+            data={accounts}
+            disabled={isDisabled}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default TransactionsPage;
--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -6,7 +6,9 @@ import { NewAccountSheet } from "@/features/accounts/components/NewAccountSheet"
 import { EditAccountSheet } from "@/features/accounts/components/EditAccountSheet"
 
 import { NewCategorySheet } from "@/features/categories/components/NewCategorySheet";
-import { EditCategorySheet } from "@/features/categories/components/EditCategorySheet"
+import { EditCategorySheet } from "@/features/categories/components/EditCategorySheet";
+
+import { NewTransactionSheet } from "@/features/transactions/components/NewTransactionSheet";
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -17,8 +19,11 @@ export const SheetProvider = () => {
     <>
       <NewAccountSheet />
       <EditAccountSheet />
+      
       <NewCategorySheet />
       <EditCategorySheet />
+
+      <NewTransactionSheet />
     </>
   );
 };
